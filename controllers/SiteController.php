91ec2736b992083a9ed289436986e52e97cb0d96
Necritto <onetopyr@mail.ru>
2020-07-08 16:06:32 +0300

    Feature: admin index page [ref:no-ref]

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,49 +3,46 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\Response;
-use yii\filters\VerbFilter;
 use app\models\Book;
 use app\models\Author;
 
 class SiteController extends Controller
 {
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    public function actionIndex()
-    {
-        $book = new Book();
+  /**
+   * Displays homepage.
+   *
+   * @return string
+   */
+  public function actionIndex()
+  {
+    $book = new Book();
 
-        if (
-            $book->load(Yii::$app->request->post()) && $book->save()
-        ) {
-            Yii::$app->session->setFlash('success', 'Saved');
-            return $this->refresh();
-        }
-
-        return $this->render('index', [
-            'book' => $book,
-            'books' => Book::getAll(),
-        ]);
+    if (
+      $book->load(Yii::$app->request->post()) && $book->save()
+    ) {
+      Yii::$app->session->setFlash('success', 'Saved');
+      return $this->refresh();
     }
 
-    /**
-     * Displays errors.
-     *
-     * @return string
-     */
-    public function actionError()
-    {
-        return $this->render('error');
-    }
+    return $this->render('index', [
+      'book' => $book,
+      'books' => Book::getAll(),
+    ]);
+  }
 
-    public function actionAdmin()
-    {
-        return $this->render('admin');
-    }
+  /**
+   * Displays errors.
+   *
+   * @return string
+   */
+  public function actionError()
+  {
+    return $this->render('error');
+  }
+
+  public function actionAdmin()
+  {
+    return $this->render('admin', ['authorsList' => Author::find()->all()]);
+  }
 }
--- views/site/admin.php
+++ views/site/admin.php
@@ -1,1 +1,28 @@
-<h1>Admin</h1>
+<?php
+/* @var $this yii\web\View */
+
+use yii\helpers\Url;
+use yii\helpers\Html;
+?>
+
+<h1>Authors</h1>
+
+<a href="<?php echo Url::to(['create']); ?>" class="btn btn-primary">Create new author</a>
+
+<table class="table table-condensed m-t-10">
+  <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Edit</th>
+    <th>Delete</th>
+  </tr>
+  <?php foreach ($authorsList as $author) : ?>
+    <tr>
+      <td><?php echo $author->id; ?></td>
+      <td><?php echo Html::encode($author->name); ?></td>
+      <td><a href="<?php echo Url::to(['update', 'id' => $author->id]); ?>">Edit</a></td>
+      <td><a href="<?php echo Url::to(['delete', 'id' => $author->id]); ?>">Delete</a></td>
+      <td></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
