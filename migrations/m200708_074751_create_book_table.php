8ab41c6a05c733c80fb56eb85de9a9a5de2b65dc
Necritto <onetopyr@mail.ru>
2020-07-08 10:53:50 +0300

    Feature: migration [ref:no-ref]

---
+++ migrations/m200708_074751_create_book_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%book}}`.
+ */
+class m200708_074751_create_book_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%book}}', [
+            'id' => $this->primaryKey(),
+            'author_id' => $this->integer()->notNull(),
+            'name' => $this->string()->unique()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%book}}');
+    }
+}
