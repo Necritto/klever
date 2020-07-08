0fcb89d585c1838b7d591ea176c80c35a229c4c9
Necritto <onetopyr@mail.ru>
2020-07-08 11:29:56 +0300

    Feature: foreign key [ref:no-ref]

--- migrations/m200708_074751_create_book_table.php
+++ migrations/m200708_074751_create_book_table.php
@@ -7,23 +7,50 @@ use yii\db\Migration;
  */
 class m200708_074751_create_book_table extends Migration
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp()
-    {
-        $this->createTable('{{%book}}', [
-            'id' => $this->primaryKey(),
-            'author_id' => $this->integer()->notNull(),
-            'name' => $this->string()->unique()->notNull(),
-        ]);
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function safeUp()
+  {
+    $this->createTable('{{%book}}', [
+      'id' => $this->primaryKey(),
+      'author_id' => $this->integer()->notNull(),
+      'name' => $this->string()->unique()->notNull(),
+    ]);
+    // creates index for column `author_id`
+    $this->createIndex(
+      'idx-book-author_id',
+      'book',
+      'author_id'
+    );
 
-    /**
-     * {@inheritdoc}
-     */
-    public function safeDown()
-    {
-        $this->dropTable('{{%book}}');
-    }
+    // add foreign key for table `author`
+    $this->addForeignKey(
+      'fk-book-author_id',
+      'book',
+      'author_id',
+      'author',
+      'id',
+      'CASCADE'
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function safeDown()
+  {
+    // drops foreign key for table `user`
+    $this->dropForeignKey(
+      'fk-book-author_id',
+      'book'
+    );
+
+    // drops index for column `author_id`
+    $this->dropIndex(
+      'idx-book-author_id',
+      'book'
+    );
+    $this->dropTable('{{%book}}');
+  }
 }
